60ee33f4fb964ab4247ff8d3ac49118ff095b2f5
Georgi Vachev <wachhew96@gmail.com>
2024-12-22 20:37:36 +0200

    fix: added underline before ChoiceScreen properties names

--- frontend/src/pixi/pixiComponents/ChoiceScreen.ts
+++ frontend/src/pixi/pixiComponents/ChoiceScreen.ts
@@ -4,40 +4,40 @@ import { gsap } from "gsap";
 export default class ChoiceScreen {
     static readonly BUTTON_HEIGHT = 100;
 
-    protected app: PIXI.Application;
-    protected overlay: PIXI.Graphics | null = null;
-    protected cardContainer: PIXI.Container | null = null;
-    protected pickButton: PIXI.Container | null = null;
-    protected fightButton: PIXI.Container | null = null;
-    protected originalParent: PIXI.Container | null = null;
-    protected originalPosition: { x: number; y: number } | null = null;
-    protected cardName: string = "";
-    protected onPick: ((name: string) => void) | null = null;
-    protected onFight: ((name: string) => void) | null = null;
+    protected _app: PIXI.Application;
+    protected _overlay: PIXI.Graphics | null = null;
+    protected _cardContainer: PIXI.Container | null = null;
+    protected _pickButton: PIXI.Container | null = null;
+    protected _fightButton: PIXI.Container | null = null;
+    protected _originalParent: PIXI.Container | null = null;
+    protected _originalPosition: { x: number; y: number } | null = null;
+    protected _cardName: string = "";
+    protected _onPick: ((name: string) => void) | null = null;
+    protected _onFight: ((name: string) => void) | null = null;
 
     constructor(app: PIXI.Application) {
-        this.app = app;
+        this._app = app;
     }
 
-    public show(card: PIXI.Container, cardName: string, onPick: (name: string) => void, onFight: (name: string) => void): void {
-        this.onPick = onPick;
-        this.onFight = onFight;
+    public show(card: PIXI.Container, _cardName: string, onPick: (name: string) => void, onFight: (name: string) => void): void {
+        this._onPick = onPick;
+        this._onFight = onFight;
 
-        this.originalParent = card.parent;
-        this.originalPosition = { x: card.x, y: card.y };
-        this.originalParent?.removeChild(card);
-        this.cardName = cardName;
+        this._originalParent = card.parent;
+        this._originalPosition = { x: card.x, y: card.y };
+        this._originalParent?.removeChild(card);
+        this._cardName = _cardName;
 
-        if (this.overlay) {
-            this.overlay.visible = true
+        if (this._overlay) {
+            this._overlay.visible = true
         } else {
-            this.overlay = new PIXI.Graphics();
-            this.overlay.rect(0, 0, this.app.renderer.width, this.app.renderer.height);
-            this.overlay.fill(0x000000, 0.5);
-            this.overlay.interactive = true;
-            this.app.stage.addChild(this.overlay);
+            this._overlay = new PIXI.Graphics();
+            this._overlay.rect(0, 0, this._app.renderer.width, this._app.renderer.height);
+            this._overlay.fill(0x000000, 0.5);
+            this._overlay.interactive = true;
+            this._app.stage.addChild(this._overlay);
 
-            this.overlay.on("pointerdown", () => {
+            this._overlay.on("pointerdown", () => {
                 card.interactive = true;
                 this.hide();
             });
@@ -45,16 +45,16 @@ export default class ChoiceScreen {
 
         card.pivot.set(card.width / 2, card.height / 2);
 
-        this.cardContainer = new PIXI.Container();
-        this.overlay.addChild(this.cardContainer);
+        this._cardContainer = new PIXI.Container();
+        this._overlay.addChild(this._cardContainer);
 
-        this.cardContainer.addChild(card);
+        this._cardContainer.addChild(card);
 
         const cardProps = { x: card.x, y: card.y, scaleX: card.scale.x, scaleY: card.scale.y };
 
         gsap.to(cardProps, {
-            x: this.app.renderer.width / 2,
-            y: this.app.renderer.height / 2 - ChoiceScreen.BUTTON_HEIGHT / 2,
+            x: this._app.renderer.width / 2,
+            y: this._app.renderer.height / 2 - ChoiceScreen.BUTTON_HEIGHT / 2,
             scaleX: 2,
             scaleY: 2,
             duration: 0.25,
@@ -63,9 +63,9 @@ export default class ChoiceScreen {
                 card.scale.set(cardProps.scaleX, cardProps.scaleY);
             },
             onComplete: () => {
-                if (this.pickButton && this.fightButton) {
-                    this.pickButton.visible = true;
-                    this.fightButton.visible = true;
+                if (this._pickButton && this._fightButton) {
+                    this._pickButton.visible = true;
+                    this._fightButton.visible = true;
                 } else {
                     this.createButtons();
                 }
@@ -74,19 +74,19 @@ export default class ChoiceScreen {
     }
 
     public hide(): void {
-        if (this.cardContainer && this.cardContainer.children.length > 0 && this.originalParent) {
-            const card = this.cardContainer.children[0];
-            card.position.set(this.originalPosition!.x, this.originalPosition!.y);
+        if (this._cardContainer && this._cardContainer.children.length > 0 && this._originalParent) {
+            const card = this._cardContainer.children[0];
+            card.position.set(this._originalPosition!.x, this._originalPosition!.y);
             card.scale.set(1);
             card.interactive = true;
-            this.originalParent.addChild(card);
-            this.cardContainer.removeChild(card);
+            this._originalParent.addChild(card);
+            this._cardContainer.removeChild(card);
         }
 
-        this.overlay!.visible = false;
-        this.pickButton!.visible = false
-        this.fightButton!.visible = false
-        this.app!.stage.interactive = true;
+        this._overlay!.visible = false;
+        this._pickButton!.visible = false
+        this._fightButton!.visible = false
+        this._app!.stage.interactive = true;
     }
 
     protected createButtons(): void {
@@ -97,69 +97,69 @@ export default class ChoiceScreen {
             fontWeight: "bold",
         });
 
-        const buttonWidth = this.cardContainer!.children[0].width / 2;
+        const buttonWidth = this._cardContainer!.children[0].width / 2;
         const buttonHeight = ChoiceScreen.BUTTON_HEIGHT;
 
-        this.pickButton = new PIXI.Container();
+        this._pickButton = new PIXI.Container();
         const pickButtonBackground = new PIXI.Graphics();
 
         pickButtonBackground.roundRect(0, 0, buttonWidth, buttonHeight, 10);
         pickButtonBackground.fill(0x54bf66);
-        this.pickButton.addChild(pickButtonBackground);
+        this._pickButton.addChild(pickButtonBackground);
 
         const pickButtonText = new PIXI.Text({ text: "PICK IT", style: buttonStyle });
 
         pickButtonText.anchor.set(0.5);
         pickButtonText.position.set(buttonWidth / 2, buttonHeight / 2);
-        this.pickButton.addChild(pickButtonText);
+        this._pickButton.addChild(pickButtonText);
 
-        this.pickButton.interactive = true;
-        this.pickButton.cursor = "pointer";
-        this.pickButton.position.set(this.app.renderer.width / 2 - buttonWidth, this.app.renderer.height - 180);
+        this._pickButton.interactive = true;
+        this._pickButton.cursor = "pointer";
+        this._pickButton.position.set(this._app.renderer.width / 2 - buttonWidth, this._app.renderer.height - 180);
 
-        this.pickButton.on("pointerover", () => {
+        this._pickButton.on("pointerover", () => {
             console.error('pointerover')
             pickButtonBackground.tint = 0x00ff00;
         });
-        this.pickButton.on("pointerout", () => {
+        this._pickButton.on("pointerout", () => {
             pickButtonBackground.tint = 0xffffff;
         });
 
-        this.pickButton.on("pointerdown", () => {
-            this.onPick?.(this.cardName);
+        this._pickButton.on("pointerdown", () => {
+            this._onPick?.(this._cardName);
             this.hide();
         });
 
-        this.app.stage.addChild(this.pickButton);
+        this._app.stage.addChild(this._pickButton);
 
-        this.fightButton = new PIXI.Container();
+        this._fightButton = new PIXI.Container();
         const fightButtonBackground = new PIXI.Graphics();
 
         fightButtonBackground.roundRect(0, 0, buttonWidth, buttonHeight, 10);
         fightButtonBackground.fill(0xe76f51);
-        this.fightButton.addChild(fightButtonBackground);
+        this._fightButton.addChild(fightButtonBackground);
 
         const fightButtonText = new PIXI.Text({ text: "FIGHT IT", style: buttonStyle });
         fightButtonText.anchor.set(0.5);
         fightButtonText.position.set(buttonWidth / 2, buttonHeight / 2);
-        this.fightButton.addChild(fightButtonText);
+        this._fightButton.addChild(fightButtonText);
 
-        this.fightButton.interactive = true;
-        this.fightButton.cursor = "pointer";
-        this.fightButton.position.set(this.app.renderer.width / 2, this.app.renderer.height - 180);
+        this._fightButton.interactive = true;
+        this._fightButton.cursor = "pointer";
+        this._fightButton.position.set(this._app.renderer.width / 2, this._app.renderer.height - 180);
 
-        this.fightButton.on("pointerover", () => {
+        this._fightButton.on("pointerover", () => {
             fightButtonBackground.tint = 0xff0000;
         });
-        this.fightButton.on("pointerout", () => {
+        this._fightButton.on("pointerout", () => {
             fightButtonBackground.tint = 0xffffff;
         });
 
-        this.fightButton.on("pointerdown", () => {
-            this.onFight?.(this.cardName);
+        this._fightButton.on("pointerdown", () => {
+            this._onFight?.(this._cardName);
             this.hide();
         });
 
-        this.app.stage.addChild(this.fightButton);
+        this._app.stage.addChild(this._fightButton);
     }
 }
