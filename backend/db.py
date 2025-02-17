752e18c528f0fb29dac0a891dd6246e4745a1f04
Georgi Vachev <44528893+Georgi-Vachev@users.noreply.github.com>
2025-02-17 18:11:48 +0200

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -1,7 +1,7 @@
 
 from pymongo import MongoClient
 
-client = MongoClient('mongodb+srv://gvachev:669574@cluster0.f8m81.mongodb.net/')
+client = MongoClient('mongodb+srv://gvachev:123456@cluster0.f8m81.mongodb.net/')
 db = client["FetchThemAll"]
 users_collection = db["users"]
 characters_collection = db["characters"]  # Collection for characters
